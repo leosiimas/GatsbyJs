a32c2e826fdaa02487a102a10089d67561380876
Leonardo Abreu <leonardo.abreu@inf.ufrgs.br>
2020-02-09 00:38:51 -0300

    feat sidebar error

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import SideBar from '../SideBar'
+import SideBar from '../Sidebar'
 import MenuBar from '../MenuBar'
 import { TransitionPortal } from 'gatsby-plugin-transition-link'
 
